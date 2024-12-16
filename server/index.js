65d9ed45b302af26dbc7b3f9b29defebc4d59c8e
bayan2712 <albrtmanybyan@gmail.com>
2024-12-16 12:47:14 +0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,8 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 
-const connectString =
-  "mongodb+srv://admin:admin@postitapp.omuyb.mongodb.net/postITDb?retryWrites=true&w=majority&appName=PostITApp";
+const connectString =`mongodb+srv://${ENV.DB_USER}:${ENV.DB_PASSWORD}@${ENV.DB_CLUSTER}/${ENV.DB_NAME}?retryWrites=true&w=majority&appName=PostITApp`;
 
 mongoose.connect(connectString, {
   useNewUrlParser: true,
